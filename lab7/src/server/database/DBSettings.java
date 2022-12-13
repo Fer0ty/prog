bee4ea46fa80a1e5a88776ef4f6858e768e2d192
Артемий <66246727+Ferotyy@users.noreply.github.com>
2022-12-13 17:55:22 +0300

    Update DBSettings.java

--- lab7/src/server/database/DBSettings.java
+++ lab7/src/server/database/DBSettings.java
@@ -1,12 +1,12 @@
 package server.database;
 
 public class DBSettings {
-    final static String PASSWORD = "eNiIXg15sQmP6nDf";
-    final static String LOGIN = "s334645";
+    final static String PASSWORD = "DB_PASS";
+    final static String LOGIN = "ITMO_LOGIN";
     final static String HOST = "pg";
-    final static String PORT = "5432";
+    final static String PORT = "PORT";
     final static String DEBUG_HOST = "localhost";
-    final static String DEBUG_PORT = "1099";
+    final static String DEBUG_PORT = "LOCAL_PORT";
     final static boolean DEBUG = true;
 
     static String url() {
